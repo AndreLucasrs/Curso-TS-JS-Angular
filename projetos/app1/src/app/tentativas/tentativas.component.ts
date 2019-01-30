94e593eb8d1ef860254237368ddf9eb495532e8f
André Lucas <joaopaulo@polisys.com.br>
2019-01-30 20:00:06 -0200

    App1: ajustes painel, topo e progresso - adicionado mock mensagens

--- projetos/app1/src/app/tentativas/tentativas.component.ts
+++ projetos/app1/src/app/tentativas/tentativas.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TentativasComponent implements OnInit {
 
+  //property binding [], usando nos atributos do HTML
+  //string interpolation {{}}, usando para passar os valores para o HTML 
+  //e tambem pode ser usado para passar valor aos atributos do HTML
+  //não usar os dois juntos
+
+  //exemplos sendo utilizado tentativas.component.html - property binding
+  //topo.component.html - string interpolation
+  public coracaoVazio: string = "/assets/coracao_vazio.png";
+  public coracaoCheio: string = "/assets/coracao_cheio.png";
+
   constructor() { }
 
   ngOnInit() {
