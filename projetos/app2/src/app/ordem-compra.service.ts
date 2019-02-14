90b6f45c673e3e291bfbdf5092ba04cf4db2fe74
AndreLucasrs <andrelrs.v@outlook.com>
2019-02-14 00:06:10 -0200

    adicionando metodo de compra

---
+++ projetos/app2/src/app/ordem-compra.service.ts
@@ -0,0 +1,8 @@
+import {Pedido} from './shared/pedido.model';
+
+export class OrdemCompraService {
+
+  public efefitivarCompra(pedido: Pedido): void {
+    console.log(pedido);
+  }
+}
--- projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
+++ projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {OrdemCompraService} from '../ordem-compra.service';
+import {Pedido} from '../shared/pedido.model';
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
-  styleUrls: ['./ordem-compra.component.css']
+  styleUrls: ['./ordem-compra.component.css'],
+  providers: [ OrdemCompraService ]
 })
 export class OrdemCompraComponent implements OnInit {
+
+  // pedido
+  public pedido: Pedido = new Pedido('', '', '', '');
 
   public endereco: string = '';
   public numero: string = '';
@@ -26,9 +32,11 @@ export class OrdemCompraComponent implements OnInit {
 
   public formaEstado: string = 'disabled';
 
-  constructor() { }
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
+
+    // this.ordemCompraService.efefitivarCompra();
   }
 
   public atualizaEndereco(endereco: string): void {
@@ -90,6 +98,14 @@ export class OrdemCompraComponent implements OnInit {
     } else {
       this.formaEstado = 'disabled';
     }
+  }
+
+  public confirmarCompra(): void {
+    this.pedido.endereco = this.endereco;
+    this.pedido.numero = this.numero;
+    this.pedido.complemento = this.complemento;
+    this.pedido.formaPagamento = this.formaPagamento;
+    this.ordemCompraService.efefitivarCompra(this.pedido);
   }
 
 }
