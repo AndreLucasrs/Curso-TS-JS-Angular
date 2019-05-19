23ea4e7ca16c77cd91b26568566c8ba92d9602ff
AndreLucasrs <andrelrs.v@outlook.com>
2019-05-18 22:52:00 -0300

    fim app3

--- projetos/app3/src/app/app.component.ts
+++ projetos/app3/src/app/app.component.ts
@@ -12,12 +12,6 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
 
     const config = {
-      apiKey: 'AIzaSyB0TKFsDJ8zwSLuZ8vzeifPRi154oZYZ34',
-      authDomain: 'jta-instagram-clone-edc78.firebaseapp.com',
-      databaseURL: 'https://jta-instagram-clone-edc78.firebaseio.com',
-      projectId: 'jta-instagram-clone-edc78',
-      storageBucket: 'jta-instagram-clone-edc78.appspot.com',
-      messagingSenderId: '22184565414'
     };
     firebase.initializeApp(config);
   }
